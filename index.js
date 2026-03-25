1ec93044a66ea86903c61b6c0f95fa74d4bd42fa
anvar010 <anvar010@users.noreply.github.com>
2026-03-25 11:15:46 +0400

    Save fixed script with date capping

--- index.js
+++ index.js
@@ -23,17 +23,20 @@ const markCommit = (x, y) => {
 };
 
 const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  if (n === 0) return simpleGit().push();
 
+  // Calculate a random date within the last year, ensuring it is not in the future.
+  const randomDays = random.int(0, 365);
+  let commitDate = moment().subtract(randomDays, "d");
+
+  const date = commitDate.format();
   const data = {
     date: date,
   };
   console.log(date);
+
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+    simpleGit().add([path]).commit(date, { "--date": date }, makeCommits.bind(null, --n));
   });
 };
 
